3c0110b0fc7802acac155ad14db460b99ada7f87
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-04-09 15:24:55 +0800

    Update 2073-time-needed-to-buy-tickets.cpp

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -8,6 +8,7 @@ public:
                 if(tickets[i]!=0){
                     tickets[i]--;
                     cnt++;
+                    //這很重要 避免我已經讓第k個已經=0 又繼續往後算其他的index直到尾端 這樣會有可能cnt又一直++到尾 會產生錯誤
                     if(tickets[k]==0){
                         break;
                     }
