fad717af8ab596e4031983df19b20aec9c4e2b6a
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-04-21 17:38:28 +0800

    Time: 124 ms (34.12%), Space: 81.5 MB (50.31%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> result;
+    vector<int>path;
+    void backtracking(int n,int k,int startIndex){
+        if(path.size()==k){
+            result.push_back(path);
+            return ;
+        }
+        for(int i=startIndex;i<=n;i++){
+            path.push_back(i);
+            backtracking(n,k,i+1);
+            path.pop_back();
+
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        backtracking(n,k,1);
+        return result;
+    }
+};
