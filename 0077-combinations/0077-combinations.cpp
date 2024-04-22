ad6e4500266ed835fdf7e492875777cf5504ac73
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-04-23 01:10:24 +0800

    Time: 113 ms (45.32%), Space: 81.5 MB (45.87%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -7,7 +7,7 @@ public:
             result.push_back(path);
             return ;
         }
-        for(int i=startIndex;i<=n;i++){
+        for(int i=startIndex;i<=n - (k - path.size()) + 1;i++){
             path.push_back(i);
             backtracking(n,k,i+1);
             path.pop_back();
