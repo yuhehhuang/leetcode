5af1d51078a1078681f3a6be0a7707da61eb3b42
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-04-23 01:28:12 +0800

    Time: 130 ms (29.47%), Space: 81.5 MB (45.87%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -7,12 +7,13 @@ public:
             result.push_back(path);
             return ;
         }
-        for(int i=startIndex;i<=n - (k - path.size()) + 1;i++){
+        for(int i=startIndex;i<=n ;i++){
             path.push_back(i);
             backtracking(n,k,i+1);
             path.pop_back();
 
         }
+        return ;
     }
     vector<vector<int>> combine(int n, int k) {
         backtracking(n,k,1);
