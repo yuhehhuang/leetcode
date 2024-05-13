fd9e7770067c3ee10f082644f21100da4ac99e4b
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-05-13 22:34:18 +0800

    Time: 237 ms (72.25%), Space: 93.3 MB (74.39%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,19 +1,37 @@
+class cmp {
+public:
+    bool operator()(vector<int>& a, vector<int>& b) {
+        if (a[0] == b[0]) {
+            return a[1] < b[1];
+        }
+        else {
+            return a[0] < b[0];
+        }
+    }
+};
 
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-    sort(intervals.begin(), intervals.end());
-    int num=0;
-    int right=intervals[0][1];
-    for(int i=1;i<intervals.size();i++){
-        if(right>intervals[i][0]){
-            num++;
-            right = min(right,intervals[i][1]);
+        sort(intervals.begin(), intervals.end(), cmp());
+        int ans = 0;
+        int left=intervals[0][0];
+        int right = intervals[0][1];
+        for (int i = 1; i < intervals.size(); i++) {
+            if(intervals[i][0]>=right){//不可能重疊啦
+                right=intervals[i][1];
+                left=intervals[i][0];
+            }
+            else{ //絕對重疊啦
+                ans++;
+                int tmp=intervals[i][1];
+                right=min(right,intervals[i][1]); //選尾值小 因為這樣比較不容易重疊
+                if(tmp==intervals[i][1]){
+                    left=intervals[i][0];
+                }
+            }
         }
-        else{
-            right=intervals[i][1];
-        }
-    }
-    return num;
+
+        return ans;
     }
 };
