ea652da2eebe5678e7b07d3056dab734c09e629d
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-05-13 22:40:14 +0800

    Update 0435-non-overlapping-intervals.cpp

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -17,6 +17,7 @@ public:
         int ans = 0;
         int left=intervals[0][0];
         int right = intervals[0][1];
+        //每次記住沒有重疊的這些set裡 最右邊的intervals[i] = [starti, endi] 又排好序了所以只要比最右邊那個看看有沒有重疊(因為要比的人 starti+1>starti，要重疊一定發生在目前最右邊的元素)
         for (int i = 1; i < intervals.size(); i++) {
             if(intervals[i][0]>=right){//不可能重疊啦
                 right=intervals[i][1];
@@ -30,6 +31,11 @@ public:
                     left=intervals[i][0];
                 }
             }
+        }
+
+        return ans;
+    }
+};
         }
 
         return ans;
