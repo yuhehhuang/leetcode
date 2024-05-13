7eaf4f5842ada23ca29d9c8f2385adcb9adf0011
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-05-13 21:56:41 +0800

    Time: 238 ms (70.76%), Space: 93.5 MB (30.84%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,14 +1,8 @@
-class cmp{
-    public:
- bool operator()(vector<int>&a,vector<int>&b){
-    
-    return a[0]<b[0];
- }
-};
+
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-    sort(intervals.begin(), intervals.end(), cmp());
+    sort(intervals.begin(), intervals.end());
     int num=0;
     int right=intervals[0][1];
     for(int i=1;i<intervals.size();i++){
