30a0057ab62b713c77cdbad0dce968e7690b73d5
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-05-19 23:28:31 +0800

    Update 0416-partition-equal-subset-sum.cpp

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+//题目中物品是nums[i]，重量是nums[i]，价值也是nums[i]，背包体积是sum/2
     bool canPartition(vector<int>& nums) {
         vector<int>dp(10001,0);
         int sum=0;
@@ -12,6 +13,8 @@ public:
         sum=sum/2;
         for(int i=0;i<nums.size();i++){
             for(int j=sum;j>=nums[i];j--){
+                //若背包體積比第i個物品的重量還小表示不可能購買物品i
+                //故背包體積為j時購買[0,i]的最佳解=背包體積為j時購買[0,i-1]是一樣的 所以就不用特地去做覆蓋(更新)
                 dp[j]=max(dp[j],dp[j-nums[i]]+nums[i]);
             }
         }
