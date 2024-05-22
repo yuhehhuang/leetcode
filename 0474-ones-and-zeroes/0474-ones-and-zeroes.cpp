ea2c95485ecd5b1d8c4cabaf08c727308e5ef4ce
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-05-23 00:33:07 +0800

    Update 0474-ones-and-zeroes.cpp

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -3,7 +3,7 @@ class Solution {
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
         int sz=strs.size();
-        vector<pii>count(sz);
+        vector<pii>count(sz,pair<int,int>(0,0));
         for(int i=0;i<sz;i++){
             for(int j=0;j<strs[i].size();j++){
                 if(strs[i][j]=='0'){
@@ -16,9 +16,11 @@ public:
         }
         vector<vector<vector<int>>> dp(sz,vector<vector<int>>(m+1,vector<int>(n+1,0)));
         int k1_inti=count[0].first,k2_inti=count[0].second;
+        //dp[i][j][l]:代表從index第0到i的物品中 背包大小:(0可有j個 1可有l個)，這樣的背包能裝的最大元素個數量
         //case 物件0處理(dp初始化) 當背包大小購放物件0 都設1;
         for(int j=0;j<m+1;j++){
             for(int l=0;l<n+1;l++){
+                //若背包大小夠買第0個物品 肯定買(才能最大值) 
                 if(k1_inti<=j&&k2_inti<=l){
                     dp[0][j][l]=1;
                 }
@@ -29,8 +31,10 @@ public:
             for(int j=0;j<m+1;j++){ //背包"0"的大小
                 for(int l=0;l<n+1;l++){ //背包"1"的大小
                     if(k1<=j&&k2<=l){
+                        //背包大小可以買第i個物品: max(買,不買)
                         dp[i][j][l]=max(dp[i-1][j-k1][l-k2]+1,dp[i-1][j][l]);
                     }
+                    //背包大小無法買東西，-->跟0到i-1個物品的最佳解一樣
                     else dp[i][j][l]=dp[i-1][j][l];
                 }
             }
