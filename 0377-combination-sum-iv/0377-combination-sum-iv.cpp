92c5856dfcbe6d4598dc1f211bc4149cb8c5d315
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-05-25 03:49:45 +0800

    Time: 0 ms (100%), Space: 8 MB (53.76%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -3,15 +3,16 @@ public:
     int combinationSum4(vector<int>& nums, int target) {
         //dp[i]means target=i的組合
         //dp[n]=nums[0]是最後一步湊成n+nums[1]是最後一步湊成n+nums[2]是最後一步湊成n+....+nums[n-1]是湊成最後一步的排列和--->nums是內迴圈
-        vector<int> dp(target + 1, 0);
+        vector<unsigned int> dp(target + 1, 0);
         dp[0] = 1;
-        for(int i=0;i<=target;i++){
-            for(int j=0;j<nums.size();j++){
-                if(i>=nums[j]&& dp[i] < INT_MAX - dp[i - nums[j]]){
-                    dp[i]+=dp[i-nums[j]];
+        for(int i = 0; i <= target; i++){
+            for(int j = 0; j < nums.size(); j++){
+                if(i >= nums[j] ){
+                    dp[i] += dp[i - nums[j]];
                 }
             }
         }
-        return dp[target];
+        // 確保返回值不會溢出
+        return dp[target] ;
     }
 };
