ca9c00cddaa5f4620da880cc44aa6863b7238e7d
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-05-25 19:23:39 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         //min....
+        //dp[i][j]=不花coins[i]的解and有花coins[i]的解去最小
         sort(coins.begin(),coins.end());
         vector<vector<int>>dp(coins.size(),vector<int>(amount+1,amount+1));
         for(int i=0;i<coins.size();i++){
@@ -15,9 +16,9 @@ public:
         for(int i=1;i<coins.size();i++){
             for(int j=1;j<amount+1;j++){
                 if(coins[i]<=j){
-                    dp[i][j]=min(dp[i][j],dp[i][j-coins[i]]+1);
+                    dp[i][j]=min(dp[i-1][j],dp[i][j-coins[i]]+1);
                 }
-                    dp[i][j]=min(dp[i][j],dp[i-1][j]);
+                    dp[i][j]=dp[i-1][j];
             }
         }
         return dp[coins.size()-1][amount]==amount+1?-1:dp[coins.size()-1][amount];
