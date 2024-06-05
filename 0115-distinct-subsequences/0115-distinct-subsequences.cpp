7fe38def4267c1011a04a8057e2c3ac9b6b516c5
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-06-05 20:49:34 +0800

    Time: 15 ms (75%), Space: 19.6 MB (71.15%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
     int numDistinct(string s, string t) {
-        //dp[i][j]: s:前i個(index:0~i)字母 符合 t:前j個字母的組合
-        //類似背包
-        vector<vector<unsigned int>>dp(s.size()+1,vector<unsigned int>(t.size()+1,0));
-        for(int i=0;i<s.size();i++){
-            dp[i][0]=1;//都不拿
-        }
-        for(int i=1;i<=s.size();i++){
-            for(int j=1;j<=t.size();j++){
-                if(s[i-1]==t[j-1]){
-                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+        // dp[i][j] represents the number of distinct subsequences of s[0..i] which equals t[0..j]
+        vector<vector<unsigned int>> dp(s.size(), vector<unsigned int>(t.size(), 0));
+
+        // Fill the dp table for the first column
+         for(int i=0;i<s.size();i++){
+            if(s[i]==t[0]){
+                if(i==0){
+                    dp[0][0]=1;
                 }
-                else{
-                    dp[i][j]=dp[i-1][j];
+                else
+                dp[i][0]=dp[i-1][0]+1;
+            }
+            else{
+                if(i==0){
+                    continue;
+                }
+                else dp[i][0]=dp[i-1][0];
+            }
+        }
+        // Fill the dp table for the first row
+
+        // Fill the rest of the dp table
+        for (int i = 1; i < s.size(); i++) {
+            for (int j = 1; j < t.size(); j++) {
+                if (s[i] == t[j]) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
         }
-        return dp[s.size()][t.size()];
+
+        return dp[s.size() - 1][t.size() - 1];
     }
 };
