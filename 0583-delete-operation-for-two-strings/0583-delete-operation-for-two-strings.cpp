c76ddbf48ca276624a734e592b68cb05310d3812
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-06-07 14:31:24 +0800

    Time: 11 ms (66.21%), Space: 15.1 MB (75.37%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        //ans =word1.size()+word2.size()-LCS
-        vector<vector<int>>dp(word1.size()+1,vector<int>(word2.size()+1,0));
-        //dp[i][j] means word1:index0~i-1 word2:index0~j-1的LCS
-        for(int i=1;i<=word1.size();i++){
-            for(int j=1;j<=word2.size();j++){
-                if(word1[i-1]==word2[j-1]){
+        vector<vector<int>>dp(word1.size(),vector<int>(word2.size(),0));
+        for(int i=0;i<word1.size();i++){
+            if(word1[i]==word2[0]){
+                dp[i][0]=1;
+            }
+            else{
+                if(i==0) continue;
+                else
+                dp[i][0]=dp[i-1][0];
+            }
+        }
+        for(int j=1;j<word2.size();j++){
+            if(word1[0]==word2[j]) dp[0][j]=1;
+            else{
+                dp[0][j]=dp[0][j-1];
+            }
+        }
+        for(int i=1;i<word1.size();i++){
+            for(int j=1;j<word2.size();j++){
+                if(word1[i]==word2[j]){
                     dp[i][j]=dp[i-1][j-1]+1;
                 }
                 else{
-                    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
-        return word1.size()+word2.size()-2*dp[word1.size()][word2.size()];
+        return word1.size()+word2.size()-2*dp[word1.size()-1][word2.size()-1];
     }
 };
