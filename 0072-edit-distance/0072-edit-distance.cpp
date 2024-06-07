57e59cf71e222f0efb818431339c0a93aae3ec32
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-06-07 21:41:37 +0800

    Time: 12 ms (30.7%), Space: 11.8 MB (70.73%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,19 +1,46 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        vector<vector<int>>dp(word1.size()+1,vector<int>(word2.size()+1,0));
-        for(int i=0;i<=word1.size();i++) dp[i][0]=i;
-        for(int j=0;j<=word2.size();j++) dp[0][j]=j;
-        for(int i=1;i<=word1.size();i++){
-            for(int j=1;j<=word2.size();j++){
-                if(word1[i-1]==word2[j-1]){
-                    dp[i][j]=dp[i-1][j-1];
+        //初始化
+        if(word1.size()==0&&word2.size()==0)return 0;
+        if(word1.size()==0||word2.size()==0){
+            return word1.size()==0?word2.size():word1.size();
+        }
+        vector<vector<int>>dp(word1.size(),vector<int>(word2.size(),0));
+        for(int i=0;i<word1.size();i++){
+            if(word1[i]==word2[0]){
+                if(i==0);
+                else{
+                    dp[i][0]=i;
+                }
+            }
+            else{
+                if(i==0){
+                    dp[0][0]=1;
                 }
                 else{
-                    dp[i][j]=min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]))+1;
+                    dp[i][0]=dp[i-1][0]+1;
                 }
             }
         }
-        return dp[word1.size()][word2.size()];
+        for(int j=1;j<word2.size();j++){
+            if(word1[0]==word2[j]){
+                dp[0][j]=j;
+            }
+            else{
+                dp[0][j]=dp[0][j-1]+1;
+            }
+        }
+        for(int i=1;i<word1.size();i++){
+            for(int j=1;j<word2.size();j++){
+                if(word1[i]==word2[j]){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=1+min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+                }
+            }
+        }
+        return dp[word1.size()-1][word2.size()-1];
     }
 };
