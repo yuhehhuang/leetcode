89529311def08ee3e1fedec3604439c69ea788b6
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-07-28 03:18:16 +0800

    Time: 0 ms (100%), Space: 11.3 MB (6.7%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int>nums3;
+        int i=0;
+        int j=0;
+        if(m==0){
+            nums1=nums2;
+            return;
+        }
+        while(m&&n){
+            if(nums1[i]<=nums2[j]){
+                nums3.push_back(nums1[i]);
+                i++;
+                m--;
+            }
+            else{
+                nums3.push_back(nums2[j]);
+                j++;
+                n--;
+            }
+        }
+        while(n){
+            nums3.push_back(nums2[j]);
+            j++;
+            n--;
+        }
+        while(m){
+            nums3.push_back(nums1[i]);
+            i++;
+            m--;
+        }
+        nums1=nums3;
+        return ;
+    }
+};
