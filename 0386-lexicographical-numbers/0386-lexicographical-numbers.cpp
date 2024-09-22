d47cb665606a06dbe67365aa1b33372bf5fb809b
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-09-23 00:27:30 +0800

    Time: 454 ms (5.01%), Space: 17.1 MB (33.13%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -1,19 +1,35 @@
-class Solution {
-public:
-    void solve(int count_num,int n,vector<int>& v){
-        if(count_num>n){
-            return ;
+bool Mycompare(int a,int b){
+    string str_a=to_string(a);
+    string str_b=to_string(b);
+    int tmp=min(str_a.size(),str_b.size());
+    int i=0;
+    while(i!=tmp){
+        if(str_a[i]<str_b[i]){
+            return true;
         }
-        v.push_back(count_num);
-        for(int i=0;i<=9;i++){
-            solve(count_num*10+i,n,v);
+        else if(str_a[i]>str_b[i]){
+            return false;
+        }
+        else{
+            i++;
         }
     }
+    if(str_a.size()==tmp){
+        return true;
+    }
+    else{
+        return false;
+    }
+    return 0 ;
+}
+class Solution {
+public:
     vector<int> lexicalOrder(int n) {
-        vector<int> ans;
-        for(int i=1;i<=9;i++){
-            solve(i,n,ans);
+        vector<int>ans;
+        for(int i=1;i<=n;i++){
+            ans.push_back(i);
         }
+        sort(ans.begin(),ans.end(),Mycompare);
         return ans;
     }
 };
