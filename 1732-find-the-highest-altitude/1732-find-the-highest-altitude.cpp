6df17ff7ecefd05b55f3bcae84907423104226be
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-09-24 15:03:03 +0800

    Update 1732-find-the-highest-altitude.cpp

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -3,11 +3,11 @@ public:
     int largestAltitude(vector<int>& gain) {
         vector<int>tmp(gain.size()+1,0);
         int ans=0;
-        for(int i=1;i<gain.size()+1;i++){
+        for(int i=1;i<tmp.size();i++){
             tmp[i]=tmp[i-1]+gain[i-1];
-        }
-        for(int i=0;i<tmp.size();i++){
-            ans=max(ans,tmp[i]);
+            if(tmp[i]>ans){
+                ans=tmp[i];
+            }
         }
         return ans;
     }
