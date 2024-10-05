b04e2b3ab7b77069433d1aa85465e855bf543e84
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-10-05 16:37:52 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        unordered_map<char,int>mp;
-        unordered_map<char,int>copy;
-        if(s2.size()<s1.size()){
+        vector<int>s1_freq(26,0);
+        vector<int>s2_freq(26,0);
+        int window_len=s1.size();
+        if(s1.size()>s2.size()){
             return false;
         }
+        //紀錄s1出現的char頻率
         for(int i=0;i<s1.size();i++){
-            mp[s1[i]]++;
+            s1_freq[s1[i]-'a']++;
         }
-        for(int i=0;i<=s2.size()-s1.size();i++){
-            if(mp.find(s2[i])!=mp.end()){
-                copy=mp;
-                for(int j=i;j<i+s1.size();j++){
-                    if(copy.find(s2[j])!=copy.end()){
-                        if(copy[s2[j]]>1){
-                            copy[s2[j]]--;
-                        }
-                        else{
-                            copy.erase(s2[j]);
-                        }
-                    }
-                    else{
-                        break;
-                    }
-                }
-                if(copy.empty()){
-                    return true;
-                }
+        //紀錄s2從0~s1.size()-1出現的char頻率
+        for(int i=0;i<s1.size();i++){
+            s2_freq[s2[i]-'a']++;
+        }
+        for(int i=0;i<=s2.size()-window_len;i++){
+            if(s1_freq==s2_freq){
+                return true;
             }
+            s2_freq[s2[i]-'a']--;
+            s2_freq[s2[i+window_len]-'a']++;
         }
         return false;
     }
