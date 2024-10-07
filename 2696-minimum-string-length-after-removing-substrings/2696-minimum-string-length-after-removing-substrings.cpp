b589306420ba9107a16bd119645eda419ea14dc2
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-10-07 17:01:59 +0800

    Time: 9 ms (24.64%), Space: 11.6 MB (31.24%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     int minLength(string s) {
         stack<char>st;
+        if(s.size()<=1){
+            return s.size();
+        }
         for(int i=0;i<s.size();i++){
             if(!st.empty()&&(st.top()=='A'&&s[i]=='B'||st.top()=='C'&&s[i]=='D')){
                 st.pop();
