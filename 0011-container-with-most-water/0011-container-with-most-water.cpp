eddb70356a20f5cb611c11ee996f2fd368001e5a
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-10-10 00:22:21 +0800

    Update 0011-container-with-most-water.cpp

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     int maxArea(vector<int>& height) {
         //area=(index j-index i)*min(height[i],height[j]);
+        //initial 頭尾算面積
+        //思考後 若移動高度高的pointer那這樣算出來的結果一定比原本更慘(上一輪矮的主導算面積或有比上一輪小的主導又長度更短=>都比原本結果差所以不用考慮移動高度高的pointer
+        //==>都移動比較矮的才有可能得到更加解(犧牲長度換取最低水平面)
         int ans=INT_MIN;
         int i=0;
         int j=height.size()-1;
