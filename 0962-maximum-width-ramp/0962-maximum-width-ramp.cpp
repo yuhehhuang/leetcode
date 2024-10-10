3f276b1aeccc169452544ab5e67f6cabb4f15137
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-10-10 17:34:23 +0800

    Update 0962-maximum-width-ramp.cpp

--- 0962-maximum-width-ramp/0962-maximum-width-ramp.cpp
+++ 0962-maximum-width-ramp/0962-maximum-width-ramp.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int maxWidthRamp(vector<int>& nums) {
         //遍歷產生遞減stack
+        //stack有i表示index:nums[i]是nums[0]~nums[i]中最小的 所以在stack內的元素不可能當右壁(因為左邊的都比他小)
+        //所以遍歷後所以左壁可能都包含在stack了 
         stack<int>st;
         int ans=0;
         for(int i=0;i<nums.size();i++){
