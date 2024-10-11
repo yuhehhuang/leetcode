122a1bee1561e4562862d0c2f32f453860748387
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-10-11 16:32:13 +0800

    Time: 144 ms (63.23%), Space: 61.3 MB (73.54%) - LeetHub

--- 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.cpp
+++ 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.cpp
@@ -1,44 +1,37 @@
 class Solution {
 public:
-    static bool cmp(const pair<int, pair<int, bool>>& a, const pair<int, pair<int, bool>>& b) {
-        if (a.first == b.first) {
-            if(a.second.second==b.second.second){
-                return 0;
-            }
-            return a.second.second<b.second.second;
-        }
-        // 否則，按照時間排序
-        return a.first < b.first;
-    }
     int smallestChair(vector<vector<int>>& times, int targetFriend) {
         int n=times.size();
-        vector<pair<int,pair<int,bool>>>events;
-        for(int i=0;i<n;i++){
-            events.push_back({times[i][0],{i,true}});
-            events.push_back({times[i][1],{i,false}});
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>busy;//{結束時間,chair index}
+        priority_queue<int,vector<int>,greater<int>>free;//{free的chair index}
+        for(int i=0;i<n;++i){
+            times[i].push_back(i);
         }
-        sort(events.begin(),events.end(),cmp);
-        priority_queue<int,vector<int>,greater<int>>availableChair;//可用的椅子 編號小的優先
-        for(int i=0;i<n;i++){
-            availableChair.push(i);
-        }
-        vector<int>occupiedChair(n);//紀錄user們使用的椅子
-        for(int i=0;i<events.size();i++){
-            int time=events[i].first;
-            int friendIndex=events[i].second.first;
-            bool isArriving=events[i].second.second;
-            if(isArriving){
-                int chair=availableChair.top();
-                availableChair.pop();
-                occupiedChair[friendIndex]=chair;
-                if(friendIndex==targetFriend){
-                    return chair;
+        sort(times.begin(),times.end());
+        int new_chair=0;//
+        for(int i=0;i<n;++i){
+            int arrival=times[i][0];
+            int leave_time=times[i][1];
+            int fr=times[i][2];
+            //把所有在arrival time時已經結束的chair釋放
+            while(!busy.empty()&&busy.top().first<=arrival){
+                free.push(busy.top().second);
+                busy.pop();
+            }
+            if(free.empty()){
+                if(fr==targetFriend){
+                    return new_chair;
                 }
+                busy.push({leave_time,new_chair});
+                new_chair++;
             }
             else{
-                availableChair.push(occupiedChair[friendIndex]);
+                int x=free.top();
+                free.pop();
+                if(fr==targetFriend) return x;
+                busy.push({leave_time,x});
             }
         }
-        return 100;
+        return -1;
     }
 };
