f7d27a325c6c4d2a20558bc4bea23ed87e9cc175
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-10-11 12:59:02 +0800

    Time: 152 ms (53.61%), Space: 67.9 MB (31.96%) - LeetHub

--- 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.cpp
+++ 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.cpp
@@ -1,20 +1,42 @@
 class Solution {
 public:
+    static bool cmp(const pair<int, pair<int, bool>>& a, const pair<int, pair<int, bool>>& b) {
+        if (a.first == b.first) {
+            // 如果時間相同，先處理離開事件 (isArriving == false)
+            return a.second.second < b.second.second;
+        }
+        // 否則，按照時間排序
+        return a.first < b.first;
+    }
     int smallestChair(vector<vector<int>>& times, int targetFriend) {
-        int targetTime=times[targetFriend][0];
-        sort(times.begin(),times.end());
-        vector<int>chairTime(times.size());//char[i]何時free
-        for(int i=0;i<times.size();i++){//friend
-            for(int j=0;j<times.size();j++){//chair
-                if(chairTime[j]<=times[i][0]){
-                    chairTime[j]=times[i][1];
-                    if(times[i][0]==targetTime){
-                        return j;
-                    }
-                    break;
+        int n=times.size();
+        vector<pair<int,pair<int,bool>>>events;
+        for(int i=0;i<n;i++){
+            events.push_back({times[i][0],{i,true}});
+            events.push_back({times[i][1],{i,false}});
+        }
+        sort(events.begin(),events.end(),cmp);
+        priority_queue<int,vector<int>,greater<int>>availableChair;//可用的椅子 編號小的優先
+        for(int i=0;i<n;i++){
+            availableChair.push(i);
+        }
+        vector<int>occupiedChair(n);//紀錄user們使用的椅子
+        for(int i=0;i<events.size();i++){
+            int time=events[i].first;
+            int friendIndex=events[i].second.first;
+            bool isArriving=events[i].second.second;
+            if(isArriving){
+                int chair=availableChair.top();
+                availableChair.pop();
+                occupiedChair[friendIndex]=chair;
+                if(friendIndex==targetFriend){
+                    return chair;
                 }
             }
+            else{
+                availableChair.push(occupiedChair[friendIndex]);
+            }
         }
-        return 0;
+        return 100;
     }
 };
