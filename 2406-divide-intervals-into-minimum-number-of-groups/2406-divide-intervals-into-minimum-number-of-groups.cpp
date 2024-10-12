ef337ec3ce69e498fb396ec6b57d7ca197886586
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-10-12 21:42:36 +0800

    Time: 2194 ms (5.18%), Space: 548.4 MB (5.18%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
@@ -9,14 +9,17 @@ class Solution {
 public:
     int minGroups(vector<vector<int>>& intervals) {
         sort(intervals.begin(),intervals.end(),cmp);
-        priority_queue<int, vector<int>, greater<int>> que;
-        for(int i=0;i<intervals.size();i++){
-            int arrive=intervals[i][0];
-            int leave=intervals[i][1];
-            if(!que.empty()&&arrive>que.top()){//新進來的時間在原本的活動結束以後，原本活動可以丟掉
-                que.pop();
+        priority_queue<int, vector<int>, greater<int>> que;//storing each group end time
+        int ans = 1;
+		que.push(intervals[0][1]);
+        for(int i=1;i<intervals.size();++i){//在min_heap head的一定是現在這個intervals[i]最可以放進group裡的，
+            if(intervals[i][0]<=que.top()){//跑進這個if表示最有機會的group也放不下==>多一個group，並且記錄這個gruop的結束時間
+                que.push(intervals[i][1]);
             }
-            que.push(leave);
+            else{//跑進這個else表示最有機會的gruop放得下，==>更新這個group的結束時間
+                que.pop();
+                que.push(intervals[i][1]);
+            }
         }
         return que.size();
     }
