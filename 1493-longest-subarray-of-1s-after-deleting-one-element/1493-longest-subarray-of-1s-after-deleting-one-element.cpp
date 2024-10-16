0d6eacdaea109c6d6ea7830d1363b14d57469c18
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-10-16 23:16:04 +0800

    Time: 40 ms (68.23%), Space: 58.9 MB (83.79%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -4,6 +4,8 @@ public:
         int left=0,right=0;
         int ans=INT_MIN;
         int k=1;
+        //可能的subarray只有兩種可能 一種是全是1的第二種是只有一個0
+        //題目變成求至多只能一個0的最長子陣列
         while(right<nums.size()){
             if(nums[right]==0){
                 k--;
