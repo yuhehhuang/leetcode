91fb88c9242309ad193e610c0ab760f8e96cc38d
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-10-24 16:59:20 +0800

    Time: 0 ms (100%), Space: 14.6 MB (61.52%) - LeetHub

--- 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.cpp
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.cpp
@@ -12,12 +12,14 @@
 class Solution {
 public:
     bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+        //終止條件
         if(root1==NULL&&root2==NULL){
-            return true;
+            return 1;
         }
         if(root1==NULL||root2==NULL||root1->val!=root2->val){
-            return false;
+            return 0;
         }
+        //
         return flipEquiv(root1->left,root2->left)&&flipEquiv(root1->right,root2->right)||flipEquiv(root1->left,root2->right)&&flipEquiv(root1->right,root2->left);
     }
 };
