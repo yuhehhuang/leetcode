cc57153a2afe8347658a7fe730bcd5e70a27c87b
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-11-12 19:50:53 +0800

    Time: 0 ms (100%), Space: 11.5 MB (64.31%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,13 +1,45 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        vector<pair<int,int>>vec;
-        for(int i=0;i<nums.size();++i){
-            pair<int,int>node={nums[i],i};
-            vec.push_back(node);
-        }
-        sort(vec.begin(),vec.end());
-        return vec[nums.size()-1].second;
+        int n = nums.size();
 
+        if(n == 1)
+        {
+            return 0;
+        }
+
+        if(nums[0] > nums[1])
+        {
+            return 0;
+        }
+
+        if(nums[n-1] > nums[n-2])
+        {
+            return n-1;
+        }
+        int low = 1;
+        int high = n-2;
+
+        while(low <= high)
+        {
+            int mid = low + (high - low)/2;
+
+            if(nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+            {
+                return mid;
+            }
+            //下面都是mid鄰居是遞增或遞減
+
+            else if(nums[mid] > nums[mid - 1])
+            {//遞增
+                low = mid + 1;
+            }
+
+            else
+            {//遞減
+                high = mid -1;
+            }
+        }
+        return -1;
     }
 };
