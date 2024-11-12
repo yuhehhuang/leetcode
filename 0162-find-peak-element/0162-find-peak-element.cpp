b9ee119356df37be49b2725f8b8a91c7a9948242
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-11-12 19:58:22 +0800

    Time: 0 ms (100%), Space: 11.5 MB (64.31%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,45 +1,26 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int n = nums.size();
-
-        if(n == 1)
-        {
-            return 0;
-        }
-
-        if(nums[0] > nums[1])
-        {
-            return 0;
-        }
-
-        if(nums[n-1] > nums[n-2])
-        {
-            return n-1;
-        }
-        int low = 1;
-        int high = n-2;
-
-        while(low <= high)
-        {
-            int mid = low + (high - low)/2;
-
-            if(nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
-            {
-                return mid;
-            }
-            //下面都是mid鄰居是遞增或遞減
-
-            else if(nums[mid] > nums[mid - 1])
-            {//遞增
-                low = mid + 1;
-            }
-
-            else
-            {//遞減
-                high = mid -1;
+        int left = 0, right = nums.size() - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            
+            // 檢查 mid 是否為峰值
+            bool isLeftSmaller = (mid == 0 || nums[mid] > nums[mid - 1]);
+            bool isRightSmaller = (mid == nums.size() - 1 || nums[mid] > nums[mid + 1]);
+            
+            if (isLeftSmaller && isRightSmaller) {
+                return mid; // 找到峰值
+            } else if (!isLeftSmaller) {
+                // 峰值在左側
+                right = mid - 1;
+            } else {
+                // 峰值在右側
+                left = mid + 1;
             }
         }
-        return -1;
+        
+        return -1; // 根據題意，應該總能找到峰值，所以不會到這一步
     }
 };
