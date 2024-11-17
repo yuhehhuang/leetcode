573b284c54e78171eed1e0f94ccb84ed817a2750
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-11-18 00:29:57 +0800

    Time: 4 ms (80.67%), Space: 11.2 MB (79.81%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -6,7 +6,6 @@ public:
         int start=0;
         unordered_map<char,int>latest_index;//紀錄每個char最新一次出現在哪
         for(int end=0;end<s.size();end++){
-            //lastest_index[s[end]]>=start表示最右邊這個重複的不會影響目前的window,可以不用管直接算長度(我們在意的是window內不要重複 window左邊有重複地沒關係!)
             if(latest_index.find(s[end])!=latest_index.end()&&latest_index[s[end]]>=start){
                 start=latest_index[s[end]]+1;
             }
