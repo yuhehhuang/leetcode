2a3e7d9dfb91af06c499a4fe421cc48a8adf6478
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-11-19 22:55:10 +0800

    Time: 0 ms (100%), Space: 14 MB (52.7%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,46 +1,24 @@
 class Solution {
 public:
-    int find_min(vector<int>&nums){
+    int search(vector<int>& nums, int target) {
+        int n=nums.size()-1;
         int left=0;
-        int right=nums.size()-2;
+        int right=n;
         while(left<=right){
             int mid=left+(right-left)/2;
-            if(nums[mid]<nums[nums.size()-1]){
+            if(nums[mid]>nums[n]&&target>nums[n]&&nums[mid]>=target){
+                right=mid-1;
+            }
+            else if(nums[mid]<=nums[n]&&target>nums[n]){
+                right=mid-1;
+            }
+            else if(nums[mid]<=nums[n]&&nums[mid]>=target){
                 right=mid-1;
             }
             else{
                 left=mid+1;
-            }
-        }
-        return right+1;
-    }
-    int find_target(vector<int>&nums,int left,int right,int target){
-        while(left<=right){
-            int mid=left+(right-left)/2;
-            if(nums[mid]<target){
-                left=mid+1;
-            }
-            else{
-                right=mid-1;
             }
         }
         return nums[left]==target?left:-1;
-    }
-    int search(vector<int>& nums, int target) {
-        int index=find_min(nums);
-        if(target>nums[nums.size()-1]){
-            //一定是在波段型，且target在第一段上升段
-            return find_target(nums,0,index-1,target);
-        }
-        else{
-            if(index==0){
-                //單一直線線段
-                return find_target(nums,0,nums.size()-1,target);
-            }
-            else{
-                //一定是在波段型，且target在第二段上升段
-                return find_target(nums,index,nums.size()-1,target);
-            }
-        }
     }
 };
