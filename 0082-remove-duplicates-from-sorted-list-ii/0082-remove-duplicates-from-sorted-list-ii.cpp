9f633f4eb6171211c94760011d775f3c07d9765a
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-11-26 21:32:06 +0800

    Time: 0 ms (100%), Space: 15.7 MB (20.91%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
@@ -16,7 +16,7 @@ public:
         while(cur->next&&cur->next->next){
             int val=cur->next->val;
             if(cur->next->next->val==val){
-                while(cur->next&&cur->next->val==val){
+                while(cur->next&&cur->next->val==val){//刪除重複直到cur後面沒結點或後面結點不同
                     cur->next=cur->next->next;
                 }
             }
