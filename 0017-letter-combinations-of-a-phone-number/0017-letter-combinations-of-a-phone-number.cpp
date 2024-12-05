f05e88d4dedeef1ee8a2d3039d9675491f30c134
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-12-05 23:21:23 +0800

    Time: 0 ms (100%), Space: 8.9 MB (22.74%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,38 +1,25 @@
 class Solution {
+    string mapping[10]{"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
 public:
-    string letterMap[10] = {
-        "", // 0
-        "", // 1
-        "abc", // 2
-        "def", // 3
-        "ghi", // 4
-        "jkl", // 5
-        "mno", // 6
-        "pqrs", // 7
-        "tuv", // 8
-        "wxyz", // 9
-    };
-    vector<string> result;//所有結果
-    string s; //單一結果
-    void backtracking(string digits,int index){
-        if(index==digits.size()){
-            result.push_back(s);
-            return;
-        }
-        int digit=digits[index]-'0';
-        string letters=letterMap[digit];
-        for(int i=0;i<letters.size();i++){
-            s.push_back(letters[i]);
-            backtracking(digits,index+1);
-            s.pop_back();
-        }
-
-    }
     vector<string> letterCombinations(string digits) {
         if(digits.size()==0){
-            return result;
+            return{};
         }
-        backtracking(digits,0);
-        return result;
+        vector<string>ans;
+        int n=digits.length();
+        string path(n,0);
+        auto dfs=[&](auto&&dfs,int i){
+            if(i==n){
+                ans.push_back(path);
+                return;
+            }
+            for(char c : mapping[digits[i] - '0']){
+                path[i]=c;
+                dfs(dfs,i+1);
+            }
+        };
+        dfs(dfs,0);
+        return ans;
+        
     }
 };
