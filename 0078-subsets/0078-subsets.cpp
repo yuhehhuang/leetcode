f6b150d6bf8c1e40fcb867be0423b605d1002f6a
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-12-06 08:19:31 +0800

    Time: 1 ms (48.08%), Space: 10 MB (41.92%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    vector<vector<int>> result;
-    vector<int>path;
-    void backtracking(vector<int>&nums,int startIndex){
-        result.push_back(path);
-        if(startIndex==nums.size()){
-            return;
-        }
-        for(int i=startIndex;i<nums.size();i++){
-            path.push_back(nums[i]);
-            backtracking(nums,i+1);
-            path.pop_back();
-        }
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
-        backtracking(nums,0);
-        return result;
+        vector<int>path;
+        vector<vector<int>>ans;
+        auto dfs=[&](auto&& self,int i)->void{
+            ans.push_back(path);
+            for(int j=i;j<nums.size();++j){
+                path.push_back(nums[j]);
+                self(self,j+1);
+                path.pop_back();
+            }
+        };
+        dfs(dfs,0);
+        return ans;
     }
 };
