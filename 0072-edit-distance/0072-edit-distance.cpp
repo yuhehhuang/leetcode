354fd6b7db4fc661d6423feeae30883af2c091a4
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-12-14 11:29:06 +0800

    Time: 7 ms (55.26%), Space: 12.9 MB (67.98%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,44 +1,25 @@
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        //初始化
-        if(word1.size()==0&&word2.size()==0)return 0;
-        if(word1.size()==0||word2.size()==0){
-            return word1.size()==0?word2.size():word1.size();
+        int n=word1.size();
+        int m=word2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        for(int j=0;j<=m;++j){
+            dp[0][j]=j;
         }
-        vector<vector<int>>dp(word1.size(),vector<int>(word2.size(),0));
-        for(int i=0;i<word1.size();i++){
-            if(word1[i]==word2[0]){
-                dp[i][0]=i;
-            }
-            else{
-                if(i==0){
-                    dp[0][0]=1;
-                }
-                else{
-                    dp[i][0]=dp[i-1][0]+1;
-                }
-            }
+        for(int i=0;i<=n;++i){
+            dp[i][0]=i;
         }
-        for(int j=1;j<word2.size();j++){
-            if(word1[0]==word2[j]){
-                dp[0][j]=j;
-            }
-            else{
-                dp[0][j]=dp[0][j-1]+1;
-            }
-        }
-        for(int i=1;i<word1.size();i++){
-            for(int j=1;j<word2.size();j++){
+        for(int i=0;i<n;++i){
+            for(int j=0;j<m;++j){
                 if(word1[i]==word2[j]){
-                    dp[i][j]=dp[i-1][j-1];
+                    dp[i+1][j+1]=dp[i][j];
                 }
                 else{
-                    //replace delete word1 or delete word2
-                    dp[i][j]=1+min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+                    dp[i+1][j+1]=min(min(dp[i+1][j],dp[i][j+1]),dp[i][j])+1;
                 }
             }
         }
-        return dp[word1.size()-1][word2.size()-1];
+        return dp[n][m];    
     }
 };
