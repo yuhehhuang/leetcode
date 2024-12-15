0551b964cfb0700fd70272109ff24dca924bac2b
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-12-15 09:54:25 +0800

    Time: 67 ms (55.78%), Space: 14.2 MB (42.84%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        if(nums.size()==1){
-            return 1;
-        }
+        //dp
         vector<int>dp(nums.size(),1);
-        int ans=INT_MIN;
-      for(int i=0;i<nums.size();i++){
-        for(int j=0;j<i;j++){
-            if(nums[j]<nums[i])
-            dp[i]=max(dp[i],dp[j]+1);
-            ans=max(ans,dp[i]);
+        for(int i=0;i<nums.size();++i){
+            for(int j=0;j<i;++j){
+                if(nums[j]<nums[i]){
+                    dp[i]=max(dp[i],dp[j]+1);
+                }
+            }
         }
-      }
-      return ans;  
+        sort(dp.begin(),dp.end());
+        return dp[nums.size()-1];
     }
 };
