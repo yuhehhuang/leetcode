4e465efd68328f0222ef6211928c7176ff8c6950
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-12-15 10:44:33 +0800

    Time: 79 ms (46.86%), Space: 14.4 MB (38.5%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
+    static bool cmp(int a,int b){
+        return a>b;
+    }
     int lengthOfLIS(vector<int>& nums) {
         //dp
-        vector<int>dp(nums.size(),1);
+        vector<int>dp(nums.size(),1);//每個值至少有自己當作結果(len=1);
         for(int i=0;i<nums.size();++i){
             for(int j=0;j<i;++j){
                 if(nums[j]<nums[i]){
@@ -10,7 +13,7 @@ public:
                 }
             }
         }
-        sort(dp.begin(),dp.end());
-        return dp[nums.size()-1];
+        sort(dp.begin(),dp.end(),cmp);
+        return dp[0];
     }
 };
