0b018740d1c388c150fa1f661fe397e82f11a1bb
Fumiyoshi <114764348+yuhehhuang@users.noreply.github.com>
2024-12-22 22:38:42 +0800

    Time: 0 ms (100%), Space: 8.8 MB (13.91%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int shift_size=haystack.size()-needle.size();
-        if(shift_size<0){
-            return -1;
+        int needleLength=needle.length();
+        if(needleLength==0)return 0;
+        int* next = new int[needleLength](); 
+        //left:longest prefix的後一個元素(longest prefix的len)，right:longest suffix的後一個元素
+        for(int right=1,left=0;right<needle.size();++right){
+            while(left>0&&needle[left]!=needle[right]){
+                left=next[left-1];
+            }
+            if(needle[left]==needle[right]){
+                left++;
+            }
+            next[right]=left;
         }
-        for(int i=0;i<=shift_size;i++){
-            if(haystack[i]==needle[0]){
-                int j;
-                for(j=i;j<i+needle.size();j++){
-                    if(haystack[j]!=needle[j-i]){
-                        break;
-                    }
-                }
-                if(j==i+needle.size()){
-                    return i;
-                }
+        for(int i=0,j=0;i<haystack.length();++i){
+            while(j>0&&haystack[i]!=needle[j]){
+                j=next[j-1];//j>0是為了避免[j-1] overflow，
+                //j代表遇到不一樣的時候 needle要用哪個index的字來跟現在的i比
+            }
+            if(haystack[i]==needle[j]){
+                j++;
+            }
+            if(j==needleLength){
+                return i-needleLength+1;
             }
         }
         return -1;
