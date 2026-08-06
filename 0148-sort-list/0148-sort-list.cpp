7c3461686c55b6c0f649765288de81c392d6f94e
Richard Huang <yuheh.huangv@gmail.com>
2026-08-06 16:23:54 +0800

    Time: 11 ms (81.58%), Space: 57.1 MB (83.03%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -9,20 +9,43 @@
  * };
  */
 class Solution {
+    ListNode* middleList(ListNode* head){
+        ListNode* slow=head;
+        ListNode* fast = head;
+        ListNode* pre=head;
+        while(fast &&fast->next){
+            pre=slow;
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        pre->next=nullptr;
+        return slow;
+    }
+    ListNode* mergetwoList(ListNode* list1 , ListNode* list2){
+        ListNode dummy;
+        ListNode *cur=&dummy;
+        while(list1 && list2){
+            if(list1->val < list2->val){
+                cur->next=list1;
+                list1=list1->next;
+            }
+            else{
+                cur->next=list2;
+                list2=list2->next;
+            }
+            cur=cur->next;
+        }
+        cur->next= list1?list1:list2;
+        return dummy.next;
+    }
 public:
     ListNode* sortList(ListNode* head) {
-    vector<int>a;
-    ListNode* tmp1 =head;
-    ListNode* tmp2 =head;
-    while(tmp1!=NULL){
-        a.push_back(tmp1->val);
-        tmp1=tmp1->next;
-    }
-    sort(a.begin(),a.end());
-    for(int i=0;i<a.size();++i){
-        tmp2->val=a[i];
-        tmp2=tmp2->next;
-    }
-    return head;
+        if(head==nullptr||head->next==nullptr){
+            return head;
+        }
+        ListNode* head2= middleList(head);
+        head = sortList(head);
+        head2 = sortList(head2);
+        return mergetwoList(head,head2);
     }
 };
