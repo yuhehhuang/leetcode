3cb7145afd5add3512031fd080e582adebe455ea
Richard Huang <yuheh.huangv@gmail.com>
2026-08-09 22:17:01 +0800

    Time: 0 ms (100%), Space: 9 MB (7.81%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.c
+++ 0190-reverse-bits/0190-reverse-bits.c
@@ -1,9 +1,10 @@
 int reverseBits(int n) {
     unsigned int ans=0;
-    for(int i=0;i<32;++i){
-        ans <<=1;
+    for(int i=0;i<31;++i){
         ans |= (n & 1);
+        ans <<= 1;
         n >>=1;
     }
+    ans |=(n & 1);
     return (int)ans;
 }
