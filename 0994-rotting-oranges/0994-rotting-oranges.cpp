1b0672894421c0e6e11b8fdcf0863d8584ef61f1
Richard Huang <yuheh.huangv@gmail.com>
2026-08-11 05:53:06 +0800

    Time: 0 ms (100%), Space: 16.7 MB (70.31%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,49 +1,38 @@
 class Solution {
+    int directions[4][2]={{-1,0},{1,0},{0,-1},{0,1}};
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        vector<vector<int>>visited=grid;
+        //建立一個quueue，放rotten orange座標(且是還沒往外汙染新鮮orange);
+        int ans=0;
+        int m=grid.size(),n=grid[0].size();
+        int fresh=0;
         queue<pair<int,int>>que;
-        int cntFreshOrange=0;
-        //把壞掉的橘子放進隊列，好橘子有幾個記起來
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(visited[i][j]==2){
-                    que.push({i,j});
+        for(int i=0;i<m;++i){
+            for(int j=0;j<n;++j){
+                if(grid[i][j]==1){
+                    fresh++;
                 }
-                if(visited[i][j]==1){
-                    cntFreshOrange++;
+                else if(grid[i][j]==2){
+                    que.push({i,j});
                 }
             }
         }
-        //
-        if(cntFreshOrange==0){
-            return 0;
-        }
-        int minute=-1;
-        vector<int>dir_row={0,1,0,-1};
-        vector<int>dir_col={1,0,-1,0};
-        while(!que.empty()){
-            int size=que.size();
-            for(int i=0;i<size;i++){
-                pair<int,int>node=que.front();
+        while(fresh && !que.empty()){
+            ans++;
+            int len=que.size();
+            for(int x=0;x<len;++x){
+                pair<int,int>tmp=que.front();
                 que.pop();
-                for(int i=0;i<4;i++){
-                    int next_row=node.first+dir_row[i];
-                    int next_col=node.second+dir_col[i];
-                    if(next_row>=0&&next_row<m&&next_col>=0&&next_col<n&&visited[next_row][next_col]==1){
-                        visited[next_row][next_col]=2;
-                        cntFreshOrange--;
-                        que.push({next_row,next_col});
+                for(int i=0;i<4;++i){
+                    int index_x=tmp.first+directions[i][0],index_y=tmp.second+directions[i][1];
+                    if(index_x>=0&&index_x<m&&0<=index_y&&index_y<n&&grid[index_x][index_y]==1){
+                        fresh--;
+                        grid[index_x][index_y]=2;
+                        que.push({index_x,index_y});
                     }
                 }
             }
-            minute++;
         }
-        if(cntFreshOrange==0){
-            return minute;
-        }
-        return -1;
+        return fresh?-1:ans;
     }
 };
