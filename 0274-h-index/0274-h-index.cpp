659f9218adf9aa0265038d4857129bc000b3b336
Richard Huang <yuheh.huangv@gmail.com>
2026-08-17 10:35:46 +0800

    Time: 2 ms (14.96%), Space: 12.3 MB (35.23%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    static bool cmp(int a,int b){
+        return a>b;
+    }
+    int hIndex(vector<int>& citations) {
+        int h_index=0;
+        sort(citations.begin(),citations.end(),cmp);
+        for(int i=0;i<citations.size();++i){
+            if(citations[i]>=h_index+1){
+                h_index++;
+            }
+        }
+        return h_index;
+    }
+};
