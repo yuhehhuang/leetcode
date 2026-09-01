50f77fa1a7a04d93edfb0beafcba8c5007472c66
Richard Huang <yuheh.huangv@gmail.com>
2026-09-01 18:00:06 +0800

    Time: 4 ms (20.85%), Space: 23.9 MB (7.48%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+         unordered_map<int,int>mp;
+         for(int i=0;i<nums.size();++i){
+            mp[nums[i]]++;
+         }
+         for(unordered_map<int,int>::iterator it=mp.begin();it!=mp.end();it++){
+            if(it->second==1){
+                return it->first;
+            }
+        }
+        return 0;
+    }
+};
