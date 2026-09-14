324cbcc1520649f91fd2408f1bcc2a4d4281cead
Richard Huang <yuheh.huangv@gmail.com>
2026-09-15 02:18:36 +0800

    Time: 0 ms (100%), Space: 16.3 MB (94.99%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -11,25 +11,27 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if(head==NULL || head->next==NULL || k==0){
+        if(head==nullptr||head->next==nullptr||k==0){
             return head;
         }
-        int length=1;
-        ListNode* tail = head;
-        while (tail->next != NULL) {
-            tail = tail->next;
-            length++;
+        //先建立閉環
+        //找到新的尾巴，head=新尾->next; 新尾->next=NULL;
+        int len=1;
+        ListNode* tmp=head;
+        while(tmp->next){
+            len++;
+            tmp=tmp->next;
         }
-        k = k % length;
-        if (k == 0) return head; // 如果 k 為 0，不需要旋轉
-        // 找到新的尾節點
-        ListNode* newTail = head;
-        for (int i = 0; i < length - k - 1; i++) {
-            newTail = newTail->next;
+        ListNode*tail=tmp;
+        tail->next=head;//建立閉環
+        int true_rot=k%len;
+        ListNode* newtail=head;
+        int x=len-true_rot-1;
+        while(x--){
+            newtail=newtail->next;
         }
-        ListNode* newHead = newTail->next;
-        newTail->next = NULL;
-        tail->next = head;
-        return newHead;
+        head=newtail->next;
+        newtail->next=NULL;
+        return head;
     }
 };
