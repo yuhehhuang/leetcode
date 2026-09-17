3438310749f8c4aa6a696734f0059c7ff51deb1a
Richard Huang <yuheh.huangv@gmail.com>
2026-09-17 18:28:03 +0800

    Time: 763 ms (28.64%), Space: 11.6 MB (37.82%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<pair<int,int>>dir={{-1,0},{1,0},{0,-1},{0,1}};
+    bool ans=false;
+    void search(int row,int col,int index,vector<vector<char>>&board,string word){
+        if(index==word.length()){
+            ans=true;
+            return;
+        }
+        if(row<0||row>=board.size()||col<0||col>=board[0].size()){
+            return;
+        }
+        if(word[index]==board[row][col]){
+            board[row][col]='.';
+            for(int i=0;i<4;++i){
+                search(row+dir[i].first,col+dir[i].second,index+1,board,word);
+            }
+            board[row][col]=word[index];
+        }
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        int n=word.length();
+        for(int i=0;i<board.size();++i){
+            for(int j=0;j<board[0].size();++j){
+                search(i,j,0,board,word);
+                if(ans==true)return true;
+            }
+        }
+        return false;
+    }
+};
