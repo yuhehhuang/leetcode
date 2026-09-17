c2083a0ee26469b1004e4e106d3951604befbee0
Richard Huang <yuheh.huangv@gmail.com>
2026-09-17 16:13:20 +0800

    Time: 0 ms (100%), Space: 10.4 MB (39.62%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
-    vector<vector<string>> result;
-    void backtracking(int n,int row,vector<string>chessboard){
+    vector<vector<string>>result;
+    void backtracking(int n,int row,vector<string>&chessboard){
         if(row==n){
-        result.push_back(chessboard);
-        return ;
+            result.push_back(chessboard);
+            return;
         }
         for(int col=0;col<n;col++){
-            if(isValid(row,col,chessboard,n)){
+            if(valid(row,col,chessboard,n)){
                 chessboard[row][col]='Q';
                 backtracking(n,row+1,chessboard);
                 chessboard[row][col]='.';
             }
-        }
+        } 
     }
-    bool isValid(int row,int col,vector<string>chessboard,int n){
-        for(int i=0;i<row;i++){//檢查座標上面有沒有Q
+    //因為一開始同個row都是'.'，所以同個row不用判斷有可能有重複queen
+    //檢查點上方不能有queen,左對角線不能有queen,右對角線不能有queen
+    bool valid(int row,int col,vector<string>&chessboard,int n){
+        for(int i=0;i<row;i++){
             if(chessboard[i][col]=='Q'){
                 return false;
             }
         }
-        for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--){//檢查座標左上有沒有Q
+        for(int i=row-1,j=col-1;i>=0&&j>=0;i--,j--){
             if(chessboard[i][j]=='Q'){
                 return false;
             }
         }
-        for(int i=row-1,j=col+1;i>=0&&j<n;i--,j++){//檢查座標右上有沒有Q
+        for(int i=row-1,j=col+1;i>=0&&j<n;i--,j++){
             if(chessboard[i][j]=='Q'){
                 return false;
             }
@@ -33,8 +35,8 @@ public:
         return true;
     }
     vector<vector<string>> solveNQueens(int n) {
-    vector<string>chessboard(n,string(n,'.'));
-    backtracking(n,0,chessboard);
-    return result;
+        vector<string>chessboard(n,string(n,'.'));
+        backtracking(n,0,chessboard);
+        return result;
     }
 };
