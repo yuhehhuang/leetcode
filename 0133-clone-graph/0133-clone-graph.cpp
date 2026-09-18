e9fed001b3d952529d9cbc846b3134769e4b15b2
Richard Huang <yuheh.huangv@gmail.com>
2026-09-19 03:54:41 +0800

    Time: 3 ms (68.16%), Space: 11.9 MB (80.8%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -26,14 +26,23 @@ public:
         if(node==nullptr){
             return nullptr;
         }
-        if(visited.find(node)!=visited.end()){
-            return visited[node];
+        Node* start=new Node(node->val);
+        visited[node]=start;
+        queue<Node*>que;
+        que.push(node);
+        while(!que.empty()){
+            Node* tmp = que.front();
+            que.pop();
+            vector<Node*>nei_list=tmp->neighbors;
+            for(int i=0;i<nei_list.size();++i){
+                if(visited.find(nei_list[i])==visited.end()){
+                    Node* clone=new Node(nei_list[i]->val);
+                    visited[nei_list[i]]=clone;
+                    que.push(nei_list[i]);
+                }
+                visited[tmp]->neighbors.push_back(visited[nei_list[i]]);
+            }
         }
-        Node* clone= new Node(node->val);
-        visited[node]=clone;
-        for(int i=0;i<node->neighbors.size();++i){
-            clone->neighbors.push_back(cloneGraph(node->neighbors[i]));
-        }
-        return clone;
+        return start;
     }
 };
